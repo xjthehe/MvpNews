1132717eab663078af768353bf2005430f37ac32
617490820@qq.com <5070363>
2018-06-08 10:45:09 +0800

    测试提交1comiit.

---
+++ app/src/main/java/cn/urovo/com/mvpnews/SplashActivity.java
@@ -0,0 +1,71 @@
+package cn.urovo.com.mvpnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Subscriber;
+import widget.SimpleButton;
+
+
+/**
+ * Created by rowen on 2018/2/26.
+ */
+public class SplashActivity extends AppCompatActivity{
+    @BindView(R.id.sb_skip)
+    SimpleButton mSbSkip;
+    private boolean mIsSkip = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        toHome();
+    }
+
+    /**
+     * 主页面
+     */
+    private void toHome(){
+        /////////////////////////
+        Toast.makeText(this,"xxx",Toast.LENGTH_SHORT).show();
+
+        RxHelper.countdown(5)
+                .subscribe(new Subscriber<Integer>(){
+                    @Override
+                    public void onCompleted(){
+                        _doSkip();
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        _doSkip();
+                    }
+                    @Override
+                    public void onNext(Integer integer) {
+                        mSbSkip.setText("跳过 " + integer);
+                    }
+                });
+    }
+
+    @OnClick(R.id.sb_skip)
+    public void onclick(){
+        _doSkip();
+    }
+
+    private void _doSkip(){
+        if (!mIsSkip) {
+            mIsSkip = true;
+            finish();
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            overridePendingTransition(R.anim.hold, R.anim.zoom_in_exit);
+        }
+    }
+
+}
---
+++ app/src/main/java/cn/urovo/com/mvpnews/testActivity.java
@@ -0,0 +1,11 @@
+package cn.urovo.com.mvpnews;
+
+/**
+ * Created by lenovo on 2018/6/8.
+ */
+
+public class testActivity  {
+
+
+
+}
