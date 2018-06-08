fa9e3792ef4fe9abcc90689c480861f8a121f6c2
617490820@qq.com <5070363>
2018-06-08 17:39:06 +0800

    mainactivity1comiit.

--- app/src/main/java/model/home/MainActivity.java
+++ app/src/main/java/model/home/MainActivity.java
@@ -1,11 +1,12 @@
 package model.home;
+import cn.urovo.com.mvpnews.R;
 import model.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity{
 
     @Override
     protected int attachLayoutRes() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @Override
